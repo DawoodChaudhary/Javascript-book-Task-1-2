1317aa0dcad7bb788de79c84357f64b2daf67a91
DawoodChaudhary <dawoodchaudhary8@gmail.com>
2023-09-01 02:02:47 +0500

    Javescript Book Task 1&2

---
+++ app.js
@@ -0,0 +1,31 @@
+      
+alert("hello world");
+
+var error="Error!Please enter a valid password";
+alert(error);
+
+alert("Welocom to JS land...\nHappy Coding");
+
+alert("Hello...I can run JS through my web browser's console");
+
+var username="David"
+alert(username);
+
+
+var myName = "Dawood";
+var fatherName = "Nadeem";
+alert(myName + fatherName);
+
+
+var email = "example@example.com";
+var text = "My email address is " ;
+alert(text + email);
+
+var book = "A smarter way to learn Javascript";
+var text1 = "I am trying to learn from the Book ";
+alert(text1 + book);
+
+
+
+
+
